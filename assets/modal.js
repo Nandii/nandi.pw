d81f3c31cc3811f8f12f41120d562b89b50a5089
Nandi <brandontieu@hotmail.com>
2026-04-06 21:39:50 -0700

    Update modal.js
    Fix

--- assets/modal.js
+++ assets/modal.js
@@ -1,18 +1,25 @@
 // get the modal
 var modal = document.getElementById('myModal');
-// get the image and insert it inside the modal
-var img = document.getElementsByClassName('modal_image');
-for (var i = 0; i < img.length; i++) {
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    img[i].addEventListener('click', function() {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    })
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+ 
+// binds click listeners to all .modal_image elements
+// called on page load and again after dynamic content is injected
+function bindModalImages() {
+    var img = document.getElementsByClassName('modal_image');
+    for (var i = 0; i < img.length; i++) {
+        img[i].onclick = function() {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            if (captionText) captionText.innerHTML = this.alt;
+        }
+    }
 }
-// get the < span > element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+ 
+// run on initial page load
+bindModalImages();
+ 
+// close modal when clicking outside the image
 window.onclick = (e) => {
     if (e.target == modal) {
         modal.style.display = "none";
